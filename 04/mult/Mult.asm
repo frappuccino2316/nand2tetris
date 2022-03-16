b362e7efe5305e35e1701c81b864ae697c184bea
Frappuccino001 <fumi00hiro@icloud.com>
2022-03-17 00:38:12 +0900

    アセンブリファイルを作成

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,35 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@i
+M=0
+@sum
+M=0
+
+(LOOP)
+@i
+D=M
+@R1
+D=D-A
+
+// r1の数だけ繰り返したら終了
+@END
+D;JGE
+
+// M[R0]の内容をDレジスタに格納
+@R0
+D=M
+// M[sum]にこれまでのM[sum]の数値にM[R0]の数値を足す
+@sum
+M=D+M
+
+// M[i]に1加算する
+@i
+M=M+1
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
