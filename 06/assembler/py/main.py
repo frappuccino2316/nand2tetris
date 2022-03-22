16f8b908b528acd12a83bfc1ff7a538db8aefec2
Frappuccino001 <fumi00hiro@icloud.com>
2022-03-23 01:41:18 +0900

    実装

--- 06/assembler/py/main.py
+++ 06/assembler/py/main.py
@@ -1,6 +1,9 @@
 import sys
 from os import path
+
 from lib.parser import Parser
+from lib.code import Code
+from lib.constants import A_COMMAND, C_COMMAND, L_COMMAND
 
 
 def main():
@@ -9,9 +12,29 @@ def main():
     output_path = path.dirname(input_path) + "/" + file_name + ".hack"
 
     parser = Parser(input_path)
+    code = Code()
 
-    with open(output_path, "w") as f:
-        f.write("\n".join(parser.lines))
+    machine_code = []
+
+    while parser.has_more_command():
+        if parser.command_type() == C_COMMAND:
+            dest_mnemonic = parser.dest()
+            comp_mnemonic = parser.comp()
+            jump_mnemonic = parser.jump()
+
+            dest = code.dest(dest_mnemonic)
+            comp = code.comp(comp_mnemonic)
+            jump = code.jump(jump_mnemonic)
+            machine_code.append("111" + comp + dest + jump)
+        elif parser.command_type() == A_COMMAND:
+            # machine_code.append(bin(int(parser.symbol())).split("b")[1])
+            binary = bin(int(parser.symbol())).split("b")[1].zfill(16)
+            machine_code.append(binary)
+        parser.advance()
+
+    with open(output_path, "w", encoding="utf-8") as f:
+        for machine_line in machine_code:
+            f.write(machine_line + "\n")
 
 
 if __name__ == "__main__":
