039659415fcdd4598373d4dc14284bd51096c37b
Frappuccino001 <fumi00hiro@icloud.com>
2022-03-22 20:45:39 +0900

    不要なコメントを削除

--- 06/assembler/py/main.py
+++ 06/assembler/py/main.py
@@ -5,21 +5,6 @@ def main():
     input_path = "../../add/Add.asm"
     output_path = "../../add/Add.hack"
 
-    # contents = ""
-
-    # with open(input_path) as f:
-    #     contents = f.read()
-
-    # with open(output_path, "w") as f:
-    #     content_of_lines = contents.splitlines()
-
-    #     deleted_comment_content = []
-    #     for line in content_of_lines:
-    #         if line[0:2] == "//" or line.isspace() or line == "":
-    #             continue
-    #         deleted_comment_content.append(line)
-
-    #     f.write("\n".join(deleted_comment_content))
     parser = Parser(input_path)
 
     with open(output_path, "w") as f:
