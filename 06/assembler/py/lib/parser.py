3ba892839f83aaeeadd40b82c0a7c03e8a68c005
Frappuccino001 <fumi00hiro@icloud.com>
2022-03-22 23:17:17 +0900

    コンストラクタで行に分割した配列を作成

--- 06/assembler/py/lib/parser.py
+++ 06/assembler/py/lib/parser.py
@@ -1,19 +1,21 @@
+A_COMMAND = "A_COMMAND"
+C_COMMAND = "C_COMMAND"
+L_COMMAND = "L_COMMAND"
+
+
 class Parser:
     def __init__(self, input_path):
-        self.input_path = input_path
+        self.lines = []
+        self.line_counter = 0
 
-    def delete_comment(self):
-        with open(self.input_path) as f:
+        with open(input_path) as f:
             raw_contents = f.read()
             splitted_lines = raw_contents.replace(" ", "").splitlines()
 
-            deleted_comment_lines = []
             for line in splitted_lines:
                 if line[0:2] == "//" or line.isspace() or line == "":
                     continue
                 if "//" in line:
                     index = line.find("//")
                     line = line[:index]
-                deleted_comment_lines.append(line)
-
-            return "\n".join(deleted_comment_lines)
+                self.lines.append(line)
--- 06/assembler/py/main.py
+++ 06/assembler/py/main.py
@@ -1,14 +1,17 @@
+import sys
+from os import path
 from lib.parser import Parser
 
 
 def main():
-    input_path = "../../add/Add.asm"
-    output_path = "../../add/Add.hack"
+    input_path = sys.argv[1]
+    file_name = path.basename(input_path).split(".")[0]
+    output_path = path.dirname(input_path) + "/" + file_name + ".hack"
 
     parser = Parser(input_path)
 
     with open(output_path, "w") as f:
-        f.write(parser.delete_comment())
+        f.write("\n".join(parser.lines))
 
 
 if __name__ == "__main__":
