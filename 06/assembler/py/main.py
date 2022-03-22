83d10cf1f4d8d2fbcc94003bcdf2144ebf91d4aa
Frappuccino001 <fumi00hiro@icloud.com>
2022-03-22 19:25:31 +0900

    コメントを削除

--- 06/assembler/py/main.py
+++ 06/assembler/py/main.py
@@ -1,11 +1,23 @@
 def main():
-    # 実際はこちらを変換したい
-    # path = "../../pong/Pong.asm"
-    # サンプル
-    path = "../../add/Add.asm"
-    with open(path) as f:
+    input_path = "../../add/Add.asm"
+    output_path = "../../add/Add.hack"
+
+    contents = ""
+
+    with open(input_path) as f:
         contents = f.read()
-        print(contents)
+        # print(contents)
+
+    with open(output_path, "w") as f:
+        content_of_lines = contents.splitlines()
+
+        deleted_comment_content = []
+        for line in content_of_lines:
+            if line[0:2] == "//" or line.isspace() or line == "":
+                continue
+            deleted_comment_content.append(line)
+
+        f.write("\n".join(deleted_comment_content))
 
 
 if __name__ == "__main__":
