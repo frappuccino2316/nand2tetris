c3b7ff2f7755b85a5604148de3ad56944e519968
Frappuccino001 <fumi00hiro@icloud.com>
2022-04-09 13:55:39 +0900

    missing ()

--- 08/vm/py/main.py
+++ 08/vm/py/main.py
@@ -34,8 +34,8 @@ def main():
         elif parser.command_type() == C_RETURN:
             code_writer.write_return()
         elif parser.command_type() == C_FUNCTION:
-            first_command = parser.arg1
-            second_command = parser.arg2
+            first_command = parser.arg1()
+            second_command = parser.arg2()
             code_writer.write_function(first_command, second_command)
         parser.advance()
 
